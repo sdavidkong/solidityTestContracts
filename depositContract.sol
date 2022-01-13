2e542809eaa042b1005c4356d61bb55b02a00111
S David Kong <76503030+sdavidkong@users.noreply.github.com>
2022-01-13 18:38:11 +0200

    Create depositContract.sol

---
+++ depositContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.7;
+
+contract Deposit{
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+    receive() external payable{
+    
+    }
+
+    fallback() external payable{
+
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function sendEther() public payable{
+        uint x;
+        x++;
+    }
+
+    function transferEther(address payable recipient, uint amount) public returns(bool){
+        require(owner == msg.sender, "Transfer failed, you are not the owner!");
+
+        if (amount <= getBalance()){
+            recipient.transfer(amount);
+            return true;
+        }else{
+            return false;
+        }
+        } 
+    } 
