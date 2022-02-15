99ba470ca45b945e73ed457a36ec45a761c1d5bc
S David Kong <76503030+sdavidkong@users.noreply.github.com>
2022-02-15 15:25:39 +0200

    Create ExceptionalHandling.sol
    testing assert() vs. require()

---
+++ ExceptionalHandling.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract ExceptionExample {
+    mapping(address => uint64) public balanceReceived;
+
+    function receiveMoney() public payable {
+        assert(balanceReceived[msg.sender] + uint64(msg.value) >= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += uint64(msg.value);
+    }
+
+    function withdrawMoney(address payable _to, uint64 _amount) public {
+        require(_amount <= balanceReceived[msg.sender], "You don't have enough Ether!"); 
+        assert(balanceReceived[msg.sender] >= balanceReceived[msg.sender] - _amount);
+            balanceReceived[msg.sender] -= _amount;
+            _to.transfer(_amount);
+        }
+    }
