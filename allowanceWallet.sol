cd69dedd8d1e0636002abd94537287646d4ed220
S David Kong <76503030+sdavidkong@users.noreply.github.com>
2022-02-18 23:08:43 +0200

    Update allowanceWallet.sol
    A wallet contract that allows you to give access to predetermined amounts of ETH to other people. For a weekly allowance to your children, for example.

--- allowanceWallet.sol
+++ allowanceWallet.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.1;
+pragma solidity ^0.8.7;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
@@ -8,10 +8,12 @@ contract Allowance is Ownable {
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
     }
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
 
     mapping(address => uint) public allowance;
 
     function setAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -21,6 +23,7 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
 
@@ -28,15 +31,23 @@ contract Allowance is Ownable {
 
 contract SharedWallet is Allowance {
 
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "Contract doesn't own enough money");
         if(!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
-    receive() external payable {
+    function renounceOwnership() public override onlyOwner {
+        revert("Can't do it");
+    }
 
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
